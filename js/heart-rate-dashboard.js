7b0a838d9b0ad16b4af1fd8f4e3d6a86e444445e
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2021-04-25 21:09:23 +0100

    copy RR-intervals

--- js/heart-rate-dashboard.js
+++ js/heart-rate-dashboard.js
@@ -4,7 +4,8 @@ function makeDashboard(chart){
         return Math.random()
     }
     var traces = [
-        {x: [], y:[], type:'line', name: 'Heart rate'}
+        {x: [], y:[], type:'line', name: 'Heart rate'},
+        {x: [], y:[], type: 'line', name: 'Intervals'}
     ]
     const layout = {
         showlegend: false,
@@ -14,21 +15,31 @@ function makeDashboard(chart){
     const options = {displayModeBar: false}
     
     let myPlot = null;
+    const hr = [];
+    const intervals = [];
     const processInfo = (info) => {
+        
         if(info){
             if(!myPlot){
                 chart.innerHTML = '';
                 myPlot = Plotly.plot(chart.id, traces, layout, options);
             }
-            Plotly.extendTraces(chart.id, {y:[[info.heartRate]], x: [[new Date()]]}, [0])
+            Plotly.extendTraces(chart.id, {y:[[info.heartRate]],  x: [[new Date()]]}, [0])
+            for(const v of info.intervals)intervals.push(v);
         }
     }
     const processMessage = (msg) => {
         if(!myPlot)chart.innerHTML = msg;
         else console.log(msg)
     }
+    document.body.addEventListener('copy', (e) => {
+        e.clipboardData.setData('text/plain', 'intervals\n' + intervals.join('\n'))
+        e.preventDefault();
+        e.stopPropagation();
+    })
     HeartRateMonitor(processInfo, processMessage)
 }
 
 let d = document.querySelector('#chart');
 d.addEventListener('click', () => makeDashboard(d));
+
